d0228430af411fb6e7ea752c37ef9090a4c57c80
Abdul <arahman.champ@gmail.com>
2024-10-08 10:43:19 +0530

    testing failure again-2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Brainwave_clone">
       <App />
     </Router>
   </React.StrictMode>
